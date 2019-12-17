df40ebb8ed5098f5955ba8372046e8aa4faabbe4
JDmocho <joanna.dmocho@gmail.com>
2019-12-17 22:10:00 +0100

    zadanie 3

---
+++ diff21.py
@@ -0,0 +1,16 @@
+# funkcja diff21, zwraca roznice 21 - n
+
+def diff21(n):
+    return 21 - n
+
+print ('Wywolujemy diff21(19)')
+wynik = diff21(19)
+print ('Wynik funkcji to', wynik)
+
+print ('Wywolujemy diff21(10)')
+wynik = diff21(10)
+print ('Wynik funkcji to', wynik)
+
+print ('Wywolujemy diff21(21)')
+wynik = diff21(21)
+print ('Wynik funkcji to', wynik)
