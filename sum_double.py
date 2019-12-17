8b0d9029361baf28549df0a45ee3118cad023141
JDmocho <joanna.dmocho@gmail.com>
2019-12-17 22:01:05 +0100

    trzeci program

---
+++ sum_double.py
@@ -0,0 +1,21 @@
+# funkcja sum_double zwraca sume dwoch wartosci
+# jesli dwie wartosci nie sa takie same zwroci dwukrotnosc sumy
+
+def sum_double(a, b):
+    if a == b:
+        return 2*(a+b)
+    elif a != b:
+        return a + b
+
+
+print ('Wywolujemy funkcje sum_double(1, 2)')
+wynik = sum_double(1, 2)
+print ('Wynik funkcji to', wynik)
+
+print ('Wywolujemy funkcje sum_double(3, 2)')
+wynik = sum_double(3, 2)
+print ('Wynik funkcji to', wynik)
+
+print ('Wywolujemy funkcje sum_double(2, 2)')
+wynik = sum_double(2, 2)
+print ('Wynik funkcji to', wynik)
